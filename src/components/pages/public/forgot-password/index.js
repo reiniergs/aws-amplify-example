0a238178f8e8b03f91f7737007911dc560aeef05
TahimiLeonBravo <tahimileon@gmail.com>
2018-10-15 14:47:34 -0500

    feat: implement forgot password ui

--- src/components/pages/public/forgot-password/index.js
+++ src/components/pages/public/forgot-password/index.js
@@ -1,18 +1,55 @@
 import React from 'react';
+import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Card from 'react-rainbow-components/components/Card';
+import Button from 'react-rainbow-components/components/Button';
+import Input from 'react-rainbow-components/components/Input';
+import UserIcon from './icons/userIcon';
+import BackIcon from './icons/backIcon';
+import './styles.css';
+import './media-queries.css';
 
 export default function ForgotPassword(props) {
     const {
         className,
         style,
     } = props;
+
+    function getClassName() {
+        return classnames('aws-amplify-app-forgot-password_container', className);
+    }
+
     return (
-        <div className={className} style={style} >
-            Forgot password!!
+        <section className={getClassName()} style={style}>
+            <Link className="aws-amplify-app-forgot-password_back-link" to="/home/signup">
+                <BackIcon className="aws-amplify-app-forgot-password_back-icon"/>
+                Back
+            </Link>
+            <Link to="/home">
+                <img src="/assets/rainbow-logo.svg" alt="rainbow logo" className="aws-amplify-app-forgot-password_image" />
+            </Link>
+            <p className="aws-amplify-app-forgot-password_header">Reset Password</p>
+            <Card className="aws-amplify-app-forgot-password_card">
+                <article className="aws-amplify-app-forgot-password_content">
+                    <p className="aws-amplify-app-forgot-password_message">
+                        A security code will be sent to your email address.
+                    </p>
+                    <Input
+                        label="Username"
+                        required
+                        placeholder="Enter your username"
+                        icon={<UserIcon />} />
+                    <Button
+                        variant="brand"
+                        label="Login"
+                    />
+                </article>
+            </Card>
+            <Link className="aws-amplify-app-forgot-password_link" to="/home/signin">Sign in?</Link>
             <Link to="/home/reset-password">Reset</Link>
-        </div>
-    );
+        </section>
+    )
 }
 
 ForgotPassword.propTypes = {
--- src/components/pages/public/reset-password/index.js
+++ src/components/pages/public/reset-password/index.js
@@ -1,15 +1,60 @@
 import React from 'react';
+import classnames from 'classnames';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Card from 'react-rainbow-components/components/Card';
+import Button from 'react-rainbow-components/components/Button';
+import Input from 'react-rainbow-components/components/Input';
+import LockIcon from './icons/lockIcon';
+import KeysIcon from './icons/keysIcon';
+import BackIcon from './icons/backIcon';
+import './styles.css';
+import './media-queries.css';
 
 export default function ResetPassword(props) {
     const {
         className,
         style,
     } = props;
+
+    function getClassName() {
+        return classnames('aws-amplify-app-reset-password_container', className);
+    }
+
     return (
-        <div className={className} style={style} >
-            Reset password!
-        </div>
+        <section className={getClassName()} style={style}>
+            <Link className="aws-amplify-app-reset-password_back-link" to="/home/signup">
+                <BackIcon className="aws-amplify-app-reset-password_back-icon"/>
+                Back
+            </Link>
+            <Link to="/home">
+                <img src="/assets/rainbow-logo.svg" alt="rainbow logo" className="aws-amplify-app-reset-password_image" />
+            </Link>
+            <p className="aws-amplify-app-reset-password_header">Reset Password</p>
+            <Card className="aws-amplify-app-reset-password_card">
+                <article className="aws-amplify-app-reset-password_content">
+                    <p className="aws-amplify-app-reset-password_message">
+                        We have sent you an code to your Email
+                        <strong> leandro@gmail.com </strong>
+                    </p>
+                    <Input
+                        label="Enter the CODE we sent to"
+                        required
+                        placeholder="Enter the verification code"
+                        icon={<KeysIcon />} />
+                    <Input
+                        label="Password"
+                        required
+                        placeholder="Enter the new password"
+                        icon={<LockIcon />} />
+                    <Button
+                        variant="brand"
+                        label="Login"
+                    />
+                </article>
+            </Card>
+            <Link className="aws-amplify-app-reset-password_link" to="/home/signin">Sign in?</Link>
+        </section>
     );
 }
 
